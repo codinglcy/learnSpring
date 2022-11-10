f8a2d41e2812de81cc2b53072c278233ee07b0fd
codinglcy <codinglcy@gmail.com>
2022-11-10 23:52:11 +0900

    :memo: 섹션 7. AOP

--- Spring/start/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/start/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 //import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +32,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
